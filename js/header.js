2494e8534bedf051ae20515e924d034316ddb67e
NicolasMartinsPaiva <paivanicolasmartins@gmail.com>
2025-11-29 11:04:11 -0300

    header responsivo

--- js/header.js
+++ js/header.js
@@ -1,31 +1,7 @@
-const button = document.getElementById("menu");
-const header = document.getElementById("header-principal");
-const tamanho = window.innerWidth;
-const subMenuA = document.getElementById("sub-sub-menu");
-const homeText = document.getElementById("homeText");
+    const btnMobile = document.getElementById('hamburger');
+    const menu = document.getElementById('nav-menu');
 
-function abrir() {
-if (header.style.display == "block") {
-    header.style.display = "none";
-} else {
-    header.style.display = "block";
-}
-}
-button.addEventListener("click", abrir)
-
-if (tamanho >= 800) {
-    header.style.display = "block";
-} else {
-    header.style.display = "none";
-}
-
-
-function abrirMini() {
-    if (homeText.style.display == "none") {
-        homeText.style.display = "block";
-    } else {
-        homeText.style.display = "none";
-    }
-}
-
-homeText.addEventListener("click", abrirMini);
+    btnMobile.addEventListener("click", () => {
+        menu.classList.toggle("active");
+        btnMobile.classList.toggle("active");
+    });
