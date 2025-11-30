3c2b2211664433d5f26f15f7e26a1b32ddd62424
NicolasMartinsPaiva <paivanicolasmartins@gmail.com>
2025-11-30 12:08:20 -0300

    ajustes do header

--- js/header.js
+++
@@ -1,7 +0,0 @@
-    const btnMobile = document.getElementById('hamburger');
-    const menu = document.getElementById('nav-menu');
-
-    btnMobile.addEventListener("click", () => {
-        menu.classList.toggle("active");
-        btnMobile.classList.toggle("active");
-    });
